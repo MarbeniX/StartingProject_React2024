0f0361bae8fd9bffdbb4e0a4a3cbeb89b7bedce8
Berlin Ortega <marlitros200002@gmail.com>
2024-12-23 10:51:25 -0600

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import Header from './components/Header.jsx';
+import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
 import { CORE_CONCEPTS } from './data.js';
+import { useState } from 'react';
 
 function App() {
+  const [selectedTopic, setSelecedTopic] = useState('Hi there');
+
+  function handleSelect(selectedButton){
+    setSelecedTopic(selectedButton);
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -19,6 +28,16 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {selectedTopic}
         </section>
       </main>
     </div>
